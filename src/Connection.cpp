e8ea5390082af9e4f648ba92a86356d0e6ce22f1
zhangminxian <756962537@qq.com>
2026-08-16 17:42:43 +0800

    缩进

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -13,23 +13,23 @@
 #include "Socket.h"
 
 Connection::Connection(EventLoop *loop, Socket *sock) : loop_(loop), sock_(sock) {
-  if (loop_ != nullptr) {
-    channel_ = new Channel(loop_, sock->getFd());
-    channel_->enableRead();
-    channel_->useET();
-  }
-  read_buffer_ = new Buffer();
-  send_buffer_ = new Buffer();
-  state_ = State::Connected;
+    if (loop_ != nullptr) {
+        channel_ = new Channel(loop_, sock->getFd());
+        channel_->enableRead();
+        channel_->useET();
+    }
+    read_buffer_ = new Buffer();
+    send_buffer_ = new Buffer();
+    state_ = State::Connected;
 }
 
 Connection::~Connection() {
-  if (loop_ != nullptr) {
-    delete channel_;
-  }
-  delete sock_;
-  delete read_buffer_;
-  delete send_buffer_;
+    if (loop_ != nullptr) {
+        delete channel_;
+    }
+    delete sock_;
+    delete read_buffer_;
+    delete send_buffer_;
 }
 void Connection::Read() {
     assert(state_ == State::Connected);
@@ -43,52 +43,52 @@ void Connection::Write() {
 }
 
 void Connection::ReadNonBlocking() {
-  int sockfd = sock_->getFd();
-  char buf[1024];  // 这个buf大小无所谓
-  while (true) {   // 使用非阻塞IO，读取客户端buffer，一次读取buf大小数据，直到全部读取完毕
-    memset(buf, 0, sizeof(buf));
-    ssize_t bytes_read = read(sockfd, buf, sizeof(buf));
-    if (bytes_read > 0) {
-      read_buffer_->append(buf, bytes_read);
-    } else if (bytes_read == -1 && errno == EINTR) {  // 程序正常中断、继续读取
-      printf("continue reading\n");
-      continue;
-    } else if (bytes_read == -1 &&
-               ((errno == EAGAIN) || (errno == EWOULDBLOCK))) {  // 非阻塞IO，这个条件表示数据全部读取完毕
-      break;
-    } else if (bytes_read == 0) {  // EOF，客户端断开连接
-      printf("read EOF, client fd %d disconnected\n", sockfd);
-      state_ = State::Closed;
-      break;
-    } else {
-      printf("Other error on client fd %d\n", sockfd);
-      state_ = State::Closed;
-      break;
+    int sockfd = sock_->getFd();
+    char buf[1024];  // 这个buf大小无所谓
+    while (true) {   // 使用非阻塞IO，读取客户端buffer，一次读取buf大小数据，直到全部读取完毕
+        memset(buf, 0, sizeof(buf));
+        ssize_t bytes_read = read(sockfd, buf, sizeof(buf));
+        if (bytes_read > 0) {
+        read_buffer_->append(buf, bytes_read);
+        } else if (bytes_read == -1 && errno == EINTR) {  // 程序正常中断、继续读取
+        printf("continue reading\n");
+        continue;
+        } else if (bytes_read == -1 &&
+                ((errno == EAGAIN) || (errno == EWOULDBLOCK))) {  // 非阻塞IO，这个条件表示数据全部读取完毕
+        break;
+        } else if (bytes_read == 0) {  // EOF，客户端断开连接
+        printf("read EOF, client fd %d disconnected\n", sockfd);
+        state_ = State::Closed;
+        break;
+        } else {
+        printf("Other error on client fd %d\n", sockfd);
+        state_ = State::Closed;
+        break;
+        }
     }
-  }
 }
 void Connection::WriteNonBlocking() {
-  int sockfd = sock_->getFd();
-  char buf[send_buffer_->size()];
-  memcpy(buf, send_buffer_->c_str(), send_buffer_->size());
-  int data_size = send_buffer_->size();
-  int data_left = data_size;
-  while (data_left > 0) {
-    ssize_t bytes_write = write(sockfd, buf + data_size - data_left, data_left);
-    if (bytes_write == -1 && errno == EINTR) {
-      printf("continue writing\n");
-      continue;
+    int sockfd = sock_->getFd();
+    char buf[send_buffer_->size()];
+    memcpy(buf, send_buffer_->c_str(), send_buffer_->size());
+    int data_size = send_buffer_->size();
+    int data_left = data_size;
+    while (data_left > 0) {
+        ssize_t bytes_write = write(sockfd, buf + data_size - data_left, data_left);
+        if (bytes_write == -1 && errno == EINTR) {
+        printf("continue writing\n");
+        continue;
+        }
+        if (bytes_write == -1 && errno == EAGAIN) {
+        break;
+        }
+        if (bytes_write == -1) {
+        printf("Other error on client fd %d\n", sockfd);
+        state_ = State::Closed;
+        break;
+        }
+        data_left -= bytes_write;
     }
-    if (bytes_write == -1 && errno == EAGAIN) {
-      break;
-    }
-    if (bytes_write == -1) {
-      printf("Other error on client fd %d\n", sockfd);
-      state_ = State::Closed;
-      break;
-    }
-    data_left -= bytes_write;
-  }
 }
 
 void Connection::Close() { delete_connectioin_callback_(sock_); }
